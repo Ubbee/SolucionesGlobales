32b430d545343031615ecbeb2668386394c0f2ed
Santiago Curotto <santiago.curotto.98@gmail.com>
2024-09-08 19:33:35 -0300

    arreglo de errores 2

--- src/Components/Wsp/Wsp.jsx
+++ src/Components/Wsp/Wsp.jsx
@@ -2,6 +2,6 @@ import styles from './wsp.module.css'
 
 export const Wsp = () => {
   return (
-    <a className={styles.wsp} href="https://wa.me/+5492612520758" target="_blank"><img src="src/Components/Wsp/img/wspIcon.png" alt="WhatsApp Icon" /></a>
+    <a className={styles.wsp} href="https://wa.me/+5492612520758" target="_blank"><img src="./img/wspIcon.png" alt="WhatsApp Icon" /></a>
   )
 }
